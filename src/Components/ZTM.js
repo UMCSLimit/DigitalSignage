ee0d47cc89687be44bd02075b4d398a93aaaa3ca
mprostakk <maciekprostak@gmail.com>
2019-03-30 20:30:38 +0100

    merge changes

--- src/Components/ZTM.js
+++ src/Components/ZTM.js
@@ -4,42 +4,6 @@ import axios from 'axios';
 class ZTM extends React.Component {
   constructor() {
       super();
-<<<<<<< HEAD
-
-      this.state = {
-          response : false,
-          data: [],
-          child: null
-        }
-  }
-
-  componentWillMount() {
-      axios.get('http://localhost:3001/ztm')
-      .then(({data}) => {
-          this.setState({
-          response: data.success,
-          data: data.payload
-          });
-          this.makeChild();
-      })
-      .catch(function(error) {
-          console.log(error);
-      })
-  }
-  
-  makeChild() {
-      const child = this.state.data.map((el, index) => {
-      return <div key={index}>
-          <h3>{el.title}</h3>
-          <img src={el.url} alt='ztm'/>
-      </div>
-      });
-
-      this.setState({
-          child: child
-      }) 
-=======
->>>>>>> cd1e9386e5037df122d56f52760d7b11853233ab
   }  
 
   render() {
